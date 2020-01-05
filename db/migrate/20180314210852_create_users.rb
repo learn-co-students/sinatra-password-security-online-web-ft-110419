c0fce5d3995ec52f556a41b16ea3bc02cf2a7374
LucyWeltner <lucyiweltner@gmail.com>
2020-01-05 14:50:17 -0500

    Done.

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
+  	create_table :users do |table|
+  		table.string :username 
+  		table.string :password_digest
+  	end
   end
   
   def down
+  	drop_table :users
   end
 end
