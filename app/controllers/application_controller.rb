b19635ba21d085c902e45d5ff5aa079021a6fb1a
Brandon Weaver <a7xncob@gmail.com>
2020-01-07 03:39:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,44 @@
 require "./config/environment"
 require "./app/models/user"
 class ApplicationController < Sinatra::Base
-
+  
 	configure do
 		set :views, "app/views"
 		enable :sessions
 		set :session_secret, "password_security"
 	end
-
+	
 	get "/" do
 		erb :index
 	end
-
+	
 	get "/signup" do
 		erb :signup
 	end
-
+	
 	post "/signup" do
-		#your code here!
+	  user = User.new(username: params[:username], password: params[:password])
+	  if user.save
+	    redirect "/login"
+	  else
+	    redirect "/failure"
+	  end
 	end
-
+	
 	get "/login" do
 		erb :login
 	end
-
+	
 	post "/login" do
-		#your code here!
+	  user = User.find_by(username: params[:username])
+	  if user && user.authenticate(params[:password])
+	    session[:user_id] = user.id
+	    redirect "/success"
+	  else
+	    redirect "/failure"
+	  end
 	end
-
+	
 	get "/success" do
 		if logged_in?
 			erb :success
@@ -35,7 +46,7 @@ class ApplicationController < Sinatra::Base
 			redirect "/login"
 		end
 	end
-
+	
 	get "/failure" do
 		erb :failure
 	end
@@ -44,15 +55,15 @@ class ApplicationController < Sinatra::Base
 		session.clear
 		redirect "/"
 	end
-
+	
 	helpers do
 		def logged_in?
-			!!session[:id]
+			!!session[:user_id]
 		end
-
+		
 		def current_user
-			User.find(session[:id])
+			User.find(session[:user_id])
 		end
 	end
-
+	
 end
